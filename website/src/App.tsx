95b7a21c2902dca8b5eacdcb46035576fbb9badc
taozhuowei <876593497@qq.com>
2026-04-21 11:10:26 +0800

    perf: remove eager pre-fetching of large data files in App component

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -5,22 +5,6 @@ import './index.css'
 import { AppRoutes } from './route/AppRoutes'
 
 function App() {
-  useEffect(() => {
-    void Promise.all([
-      import('./generated/knowledge'),
-      import('./generated/problems'),
-      import('./generated/test-manifest'),
-    ]).then(([knowledgeModule, problemModule, testManifestModule]) => {
-      window.__PRACTICE_DEBUG__ = {
-        problems: problemModule.problems,
-        knowledgeArticles: knowledgeModule.knowledgeArticles,
-        generatedTestManifest: testManifestModule.generatedTestManifest as NonNullable<
-          Window['__PRACTICE_DEBUG__']
-        >['generatedTestManifest'],
-      }
-    })
-  }, [])
-
   return (
     <AppStateProvider>
       <HashRouter>
